92efb18426baee55a6b340672b8c6c87368d0003
Susannah Su <susannah_su@college.harvard.edu>
2022-03-15 11:59:47 -0400

    fail buid

---
+++ bmi_calculator/lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'dart:html';
+
+import 'package:bmi_calculator/constants/app_constants.dart';
+import 'package:bmi_calculator/widgets/left_bar.dart';
+import 'package:bmi_calculator/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height:20,),
+          ],
+        ),
+      ),
+    );
+  }
+}
